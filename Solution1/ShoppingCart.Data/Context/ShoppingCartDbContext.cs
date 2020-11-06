b1d79739fed271932b3268e517c9256c35d5b06f
Liam Xuereb <Liam.Xuereb.c10275@mcast.edu.mt>
2020-11-06 15:19:26 +0100

    Migaration - Creating out first Repository

--- Solution1/ShoppingCart.Data/Context/ShoppingCartDbContext.cs
+++ Solution1/ShoppingCart.Data/Context/ShoppingCartDbContext.cs
@@ -17,5 +17,12 @@ namespace ShoppingCart.Data.Context
         public DbSet<Product> Products { get; set; }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+        }
     }
 }
